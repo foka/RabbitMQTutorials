d5ccadd757ae7129a07902fd723af288506fa3b2
Michał Włodarczyk <michal.foka.wlodarczyk@gmail.com>
2022-10-16 19:20:15 +0200

    Persist and ack Worker Queues

--- WorkQueues/NewTask/NewTask.cs
+++ WorkQueues/NewTask/NewTask.cs
@@ -9,18 +9,22 @@ class NewTask
         using (var connection = factory.CreateConnection())
         using (var channel = connection.CreateModel())
         {
-            channel.QueueDeclare(queue: "hello",
-                                 durable: false,
+            channel.QueueDeclare(queue: "task_queue",
+                                 durable: true,
                                  exclusive: false,
                                  autoDelete: false,
                                  arguments: null);
+                              
 
             var message = GetMessage(args);
             var body = Encoding.UTF8.GetBytes(message);
-
+            
+            var properties = channel.CreateBasicProperties();
+            properties.Persistent = true;
+            
             channel.BasicPublish(exchange: "",
                                  routingKey: "hello",
-                                 basicProperties: null,
+                                 basicProperties: properties,
                                  body: body);
             Console.WriteLine(" [x] Sent {0}", message);
         }

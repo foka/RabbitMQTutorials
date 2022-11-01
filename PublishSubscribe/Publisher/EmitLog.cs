22e52fdc2aa678e006cd356cd42a7b1e04005d4d
Michał Włodarczyk <michal.foka.wlodarczyk@gmail.com>
2022-11-01 19:10:41 +0100

    Add Publish/Subscribe tutorial

---
+++ PublishSubscribe/Publisher/EmitLog.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+class EmitLog
+{
+    public static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.ExchangeDeclare("logs", ExchangeType.Fanout);
+            
+            var message = GetMessage(args);
+            var body = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(exchange: "logs",
+                                 routingKey: "",
+                                 basicProperties: null,
+                                 body: body);
+            Console.WriteLine(" [x] Sent {0}", message);
+        }
+        
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();        
+    }
+    
+    private static string GetMessage(string[] args)
+    {
+        return (args.Length > 0) ? string.Join(" ", args) : "Hello World!";
+    }    
+}
